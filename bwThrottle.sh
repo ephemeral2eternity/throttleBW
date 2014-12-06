37bb36bcf670b0c670f2a16e24b82f0fef5a249c
Chen <chenw@cmu.edu>
2014-12-06 22:13:32 +0000

    fix bugs

--- bwThrottle.sh
+++ bwThrottle.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-if [$# -gt 0]
+period=30
+
+if [ $# -gt 0 ]
 then
-        period = $1
-else
-        period = 10
+       period=$1
 fi
 
 while true
 do
         sudo wondershaper eth0 1048576 1048576
         echo "--------- high bw 1Gbps -------"
-        sleep $1
-        sudo wondershaper eth0 1048576 10480
-        echo "--------- low bw 10Mbps -------"
-        sleep $1
+        sleep $period
+        sudo wondershaper eth0 1048576 1048
+        echo "--------- low bw 1Mbps -------"
+        sleep $period
 done
