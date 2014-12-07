49d9a7071e39297044c140c1e7ad945f10385dfe
Chen <chenw@cmu.edu>
2014-12-07 16:14:45 +0000

    throttle max bw to 10Mbps

--- bwThrottle.sh
+++ bwThrottle.sh
@@ -8,10 +8,10 @@ fi
 
 while true
 do
-        sudo wondershaper eth0 1048576 1048576
-        echo "--------- high bw 1Gbps -------"
+        echo "--------- high bw 10 Mbps -------"
+        sudo wondershaper eth0 1048576 10240
         sleep $period
-        sudo wondershaper eth0 1048576 1048
         echo "--------- low bw 1Mbps -------"
+        sudo wondershaper eth0 1048576 1024
         sleep $period
 done
