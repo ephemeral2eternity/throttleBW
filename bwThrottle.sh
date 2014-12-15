bed2fbba66b134311a246ec9cb592ea4df152609
Chen <chenw@cmu.edu>
2014-12-15 01:24:35 +0000

    periodically limit to 2Mbps

--- bwThrottle.sh
+++ bwThrottle.sh
@@ -11,7 +11,7 @@ do
         echo "--------- high bw 10 Mbps -------"
         sudo wondershaper eth0 1048576 10240
         sleep $period
-        echo "--------- low bw 1Mbps -------"
-        sudo wondershaper eth0 1048576 1024
+        echo "--------- low bw 2Mbps -------"
+        sudo wondershaper eth0 1048576 2048
         sleep $period
 done
