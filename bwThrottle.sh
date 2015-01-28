591493c371e4440b4283eba49f69c0a8f68847dd
Chen Wang <chenw@andrew.cmu.edu>
2015-01-28 11:55:59 +0000

    change throttling bw for CLOUD paper

--- bwThrottle.sh
+++ bwThrottle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-period=120
+period=60
 
 if [ $# -gt 0 ]
 then
@@ -8,10 +8,10 @@ fi
 
 while true
 do
-        echo "--------- high bw 10 Mbps -------"
-        sudo wondershaper eth0 1048576 10240
+        echo "--------- high bw 1 Gbps -------"
+        sudo wondershaper eth0 1048576 1048576
         sleep $period
-        echo "--------- low bw 2Mbps -------"
-        sudo wondershaper eth0 1048576 2048
+        echo "--------- low bw 10 Mbps -------"
+        sudo wondershaper eth0 1048576 10240
         sleep $period
 done
